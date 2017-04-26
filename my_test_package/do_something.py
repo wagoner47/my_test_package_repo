9700eaf9e7ae7b34bbed5a1985b298b1e98db6be
wagoner47 <wagoner47@email.arizona.edu>
2017-04-25 17:31:29 -0700

    Initial commit

---
+++ my_test_package/do_something.py
@@ -0,0 +1,18 @@
+
+def do_something(base, power):
+    """
+    Find the value of ``base``^``power``
+    
+    Parameters
+    ----------
+    base : float
+        Number to exponentiate
+    power : float
+        Power to which to raise ``base``
+        
+    Returns
+    -------
+    float
+        The result of taking ``base``^``power``
+    """ 
+    return base**power
---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='my_test_packages',
+      version='0.1dev',
+      description='A test python package',
+      author='Erika Wagoner',
+      author_email='wagoner47@email.arizona.edu',
+      packages=['my_test_package'],
+     )
